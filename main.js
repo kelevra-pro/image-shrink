14fc0c7d7071b69073a43edb3298f6c537d806ab
Damir Mukhamedinov <56725080+Kelevra-pro@users.noreply.github.com>
2020-05-29 23:19:29 +0300

    Working With The Menu (#2)
    * Creating a Menu Template
    
    * Menu & Global Shortcuts
    
    * Menu Roles
    
    * Open Window From Menu

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 
 process.env.NODE_ENV = 'development';
 
@@ -6,6 +6,7 @@ const isDev = process.env.NODE_ENV !== 'production';
 const isMac = process.platform === 'darwin';
 
 let mainWindow;
+let aboutWindow;
 
 function createMainWindow () {
   mainWindow = new BrowserWindow({
@@ -13,13 +14,70 @@ function createMainWindow () {
     width: 500,
     height: 600,
     icon: `${__dirname}/assets/icons/Icon_256x256.png`,
-    resizable: isDev
+    resizable: isDev,
+    backgroundColor: 'white'
   });
 
   mainWindow.loadFile(`${__dirname}/app/index.html`);
 }
 
-app.on('ready', createMainWindow);
+function createAboutWindow () {
+  aboutWindow = new BrowserWindow({
+    title: 'About ImageShrink',
+    width: 300,
+    height: 300,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: false,
+    backgroundColor: 'white'
+  });
+
+  aboutWindow.loadFile(`${__dirname}/app/about.html`);
+}
+
+app.on('ready', () => {
+  createMainWindow();
+
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  mainWindow.on('closed', () => mainWindow = null);
+});
+
+const menu = [
+  ...(isMac ? [
+    {
+      label: app.name,
+      submenu: [
+        {
+          label: 'About',
+          click: createAboutWindow
+        }
+      ]
+    }] : []),
+  {
+    role: 'fileMenu'
+  },
+  ...(!isMac ? [
+    {
+      label: 'Help',
+      submenu: [
+        {
+          label: 'About',
+          click: createAboutWindow
+        }
+      ]
+    }] : []),
+  ...(isDev ? [
+    {
+      label: 'Developer',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forcereload' },
+        { type: 'separator' },
+        { role: 'toggledevtools' }
+      ]
+    }] : [])
+];
 
 app.on('window-all-closed', () => {
   if (!isMac) {
