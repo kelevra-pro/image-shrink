cc7cdcce85ace038d4227714a5406ab2aa65a776
Damir Mukhamedinov <56725080+Kelevra-pro@users.noreply.github.com>
2020-07-10 17:39:31 +0300

    4-Interface, IPC, ImageMin & Packaging (#3)
    * Creating The Interface
    
    * Renderer Node Integration
    
    * IPC Communication
    
    * Implementing Imagemin & Plugins
    
    * Send Events To Renderer
    
    * Creating Log Files
    
    * Package & Test App

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+const os = require('os');
+const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron');
+const imagemin = require('imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const slash = require('slash');
+const log = require('electron-log');
 
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = 'production';
 
 const isDev = process.env.NODE_ENV !== 'production';
 const isMac = process.platform === 'darwin';
@@ -11,12 +18,19 @@ let aboutWindow;
 function createMainWindow () {
   mainWindow = new BrowserWindow({
     title: 'ImageShrink',
-    width: 500,
+    width: isDev ? 800 : 500,
     height: 600,
     icon: `${__dirname}/assets/icons/Icon_256x256.png`,
     resizable: isDev,
-    backgroundColor: 'white'
+    backgroundColor: 'white',
+    webPreferences: {
+      nodeIntegration: true
+    }
   });
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 
   mainWindow.loadFile(`${__dirname}/app/index.html`);
 }
@@ -78,6 +92,36 @@ const menu = [
       ]
     }] : [])
 ];
+
+ipcMain.on('image:minimize', (e, options) => {
+  options.dest = path.join(os.homedir(), 'imageshrink');
+  shrinkImage(options);
+});
+
+async function shrinkImage ({ imgPath, quality, dest }) {
+  try {
+    const pngQuality = quality / 100;
+
+    const files = await imagemin([slash(imgPath)], {
+      destination: dest,
+      plugins: [
+        imageminMozjpeg({ quality }),
+        imageminPngquant({
+          quality: [pngQuality, pngQuality]
+        })
+      ]
+    });
+
+    log.info(files);
+
+    shell.openPath(dest);
+
+    mainWindow.webContents.send('image:done');
+
+  } catch (err) {
+    log.error(err);
+  }
+}
 
 app.on('window-all-closed', () => {
   if (!isMac) {
